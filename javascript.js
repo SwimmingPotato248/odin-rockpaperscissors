f97782bc0a8d7fbffde09a82d1f092b858f36939
swimmingpotato <ptphong200i@gmail.com>
2022-03-21 13:32:42 +0700

    Add play one round

--- javascript.js
+++ javascript.js
@@ -10,4 +10,27 @@ function computerPlay() {
     }
 }
 
+const playerSelection = prompt("Make your move rock, paper, or scissors").toLowerCase();
 const computerSelection = computerPlay();
+
+function playRound(computerSelection, playerSelection) {
+    if (computerSelection === playerSelection) {
+        return "draw";
+    } else if (playerSelection === "rock") {
+        if (computerSelection === "scissors") {
+            return "win";
+        } else return "lose";
+    } else if (playerSelection === "paper") {
+        if (computerSelection === "rock") {
+            return "win";
+        } else return "lose";
+    } else if (playerSelection === "scissors") {
+        if (computerSelection === "paper") {
+            return "win";
+        } else return "lose";
+    }
+}
+
+console.log(playRound(computerSelection, playerSelection));
+console.log(computerSelection);
+console.log(playerSelection);
